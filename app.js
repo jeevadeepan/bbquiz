d2cb39734e38c015a201bae03527ded1dd341f54
pradeep <pradeepin2@gmail.com>
2012-11-24 23:16:53 +0530

    -a

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-var connect = require("connect");
-connect(connect.static(__dirname+"/client")).listen(8000);
+var express = require("express"),
+    app     = express(),
+    port    = parseInt(process.env.PORT, 10) || 4567;
+    
+app.get("/", function(req, res) {
+console.log("inside redirection");
+  res.redirect("/index.html");
+});
+
+
+app.get("/quiz",function(req,res){
+		console.log("inside quiz");
+	res.json({user:"pradeep"});
+});
+
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  //app.use(express.urlencoded);
+  app.use(express.static(__dirname + '/client'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(port);
