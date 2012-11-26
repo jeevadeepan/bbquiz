e49871eded9342a94a6990eb5a00232d6e56b9ff
pradeep <pradeepin2@gmail.com>
2012-11-26 21:26:19 +0530

    test implemented

---
+++ client/js/app/models/quizModel.js
@@ -0,0 +1,11 @@
+define(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
+	var quiz = Backbone.Model.extend({
+		//Set all the default properties here
+		default:{
+		
+		}
+		
+
+	});
+	return quiz;
+});
