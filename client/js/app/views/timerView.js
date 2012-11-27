b3ee7a3b8ba73636461843eb43237cd82ad3699c
pradeep <pradeepin2@gmail.com>
2012-11-27 21:20:18 +0530

    views added

---
+++ client/js/app/views/timerView.js
@@ -0,0 +1,36 @@
+define([
+				'jquery', 
+				'underscore',
+				'backbone'
+				], function($, _, Backbone){
+
+	var timerView = Backbone.View.extend({
+	
+	  //Define the element corresponding to the view here
+		el:'',
+		
+		//Define all the events here,In backbone all the events use event delegation
+		events :{
+		
+		
+		},
+		
+		/*this function will be called while creating the new instance of the view. All the thirdparty 
+		* code corresponding to the view. should be initialized here
+		*/
+		initialize: function{
+			//Best practice for having reference of the view
+			var that = this;
+		},
+		
+		/*
+		* All the templating updation should be done here, only this  should talk to the template
+		*/
+		render: function{
+		}
+	
+	});
+
+	return timerView;
+	
+});
