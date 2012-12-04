47d38228180daa8b5777bee24d147d5747fba362
pradeep <pradeepin2@gmail.com>
2012-12-04 23:05:09 +0530

    added changes to login view

--- client/js/app/views/loginView.js
+++ client/js/app/views/loginView.js
@@ -12,7 +12,7 @@ define([
 		
 		//Define all the events here,In backbone all the events use event delegation
 		events :{
-		
+			'keypress input':'updateUserName'
 		
 		},
 		
@@ -22,14 +22,26 @@ define([
 		initialize: function(){
 			//Best practice for having reference of the view
 			var that = this;
-			console.log(loginTemplate);
+			var loginModel = that.model;
+			that.render();
+			loginModel.on("change:userName",function(model,name){
+				that.$el.find('#main-content input').val(name);
+			});
 		},
 		
 		/*
 		* All the templating updation should be done here, only this  should talk to the template
 		*/
 		render: function(){
-			
+			var that = this;
+			that.$el.find('#main-header hgroup h1').html(loginTemplate.loginHeader);
+			that.$el.find('#main-content').html(loginTemplate.login);
+			that.$el.find('#actions').html(loginTemplate.loginButton);
+		},
+		
+		updateUserName:function(e){
+			var that = this;
+			that.model.set("userName",$(e.target).val());
 		}
 	
 	});
