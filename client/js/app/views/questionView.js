c4ded8124de36976d28a38c2f2778d7164c0ccd8
pradeep <pradeepin2@gmail.com>
2012-12-05 23:06:54 +0530

    added chanes w.r.t question view

--- client/js/app/views/questionView.js
+++ client/js/app/views/questionView.js
@@ -11,14 +11,13 @@ define([
 		
 		//Define all the events here,In backbone all the events use event delegation
 		events :{
-		
-		
+	
 		},
 		
 		/*this function will be called while creating the new instance of the view. All the thirdparty 
 		* code corresponding to the view. should be initialized here
 		*/
-		initialize: function{
+		initialize: function(){
 			//Best practice for having reference of the view
 			var that = this;
 		},
@@ -26,7 +25,7 @@ define([
 		/*
 		* All the templating updation should be done here, only this  should talk to the template
 		*/
-		render: function{
+		render: function(){
 		}
 	
 	});
