6ba414ae537310c969be1dba93f08ff1064abfe7
pradeep <pradeepin2@gmail.com>
2012-12-05 23:05:26 +0530

    made changes w.r.t the question view

--- client/js/app/views/pageView.js
+++ client/js/app/views/pageView.js
@@ -3,8 +3,10 @@ define([
 		 'underscore',
 		 'backbone',
 		 '/js/app/views/loginView.js',
-		 '/js/app/models/loginModel.js'
-		], function($, _, Backbone,LoginView,LoginModel){
+		 '/js/app/views/questionView.js',
+		 '/js/app/models/loginModel.js',
+		 '/js/app/collections/questionCollection.js'
+		], function($, _, Backbone,LoginView,QuestionView,LoginModel,Questions){
 
 	var pageView = Backbone.View.extend({
 	
@@ -13,8 +15,7 @@ define([
 		
 		//Define all the events here,In backbone all the events use event delegation
 		events :{
-		
-		
+			'click #startGame':'startQuiz'
 		},
 		
 		/*this function will be called while creating the new instance of the view. All the thirdparty 
@@ -23,18 +24,57 @@ define([
 		initialize: function(){
 			//Best practice for having reference of the view
 			var that = this;
-			var loginModel = new LoginModel();
-			var loginView = new LoginView({model:loginModel});
+			that.showLoginView();
 		},
 		
 		/*
 		* All the templating updation should be done here, only this  should talk to the template
 		*/
 		render: function(){
+		},
+		
+		/**
+		 * function to start the quiz by making an ajax request to the server and getting the list of 
+		 * question and options
+		 */
+		startQuiz: function(){
+			var that = this;
+			$.ajax({
+				  url: 'client/quiz.json',
+				  dataType: 'json',
+				  data: {},
+				  success: function(data){that.getAllQuestions(data);}
+				});
+			
+		},
+		
+		getAllQuestions:function(data){
+			var that=this;
+			//storing all the questions into the view.
+			that.options.questions = new Questions(data);
+			that.createQuestionView(that.options.questions.at(0));
+		},
+		
+		showLoginView:function(){
+			var loginModel = new LoginModel();
+			var loginView = new LoginView({model:loginModel});
+		},
+		
+		createQuestionView:function(questionModel){
+			var questionView = new QuestionView({model:questionModel});
+		},
+		
+		showNextQuestion:function(){
+			
+		},
+		
+		showResult:function(){
+			
 		}
+		
 	
 	});
-
+	
 	return pageView;
 	
 });
